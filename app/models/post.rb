916810b800343decb6874db3326344277e59630c
Firstname Lastname <fsbkgb667@gmail.com>
2012-03-07 19:09:42 +0400

    old threads deletion fix

--- app/models/post.rb
+++ app/models/post.rb
@@ -123,8 +123,8 @@ class Post
 
   def check_posts_length
      board = Board.find(self.board_abbreviation) 
-     if Post.find(:conditions => {:board_abbreviation => board.abbreviation}).length > board.maxthreads
-        post = Post.find(:conditions => {:board_abbreviation => board.abbreviation}).descending(:bump).last
+     if Post.all(:conditions => {:board_abbreviation => board.abbreviation}).length > board.maxthreads
+        post = Post.all(:conditions => {:board_abbreviation => board.abbreviation}).descending(:bump).last
         post.destroy
      end
   end
